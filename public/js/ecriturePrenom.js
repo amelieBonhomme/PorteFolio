01efcf119306962fd24322d88fc0181ca98a09ee
Amélie Bonhomme <amelie.bonhomme@esicad.org>
2025-10-01 21:56:57 +0200

    prénom écrit corriger

--- public/js/ecriturePrenom.js
+++ public/js/ecriturePrenom.js
@@ -1,30 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-const span = document.getElementById("nom");
-const fullText = "Amélie Bonhomme";
-let index = 0;
-let isDeleting = false;
+  const span = document.getElementById("nom");
+  const fullText = "Amélie Bonhomme";
+  let index = 0;
 
-function typeEffect() {
-    if (!isDeleting) {
-    span.textContent = fullText.slice(0, index);
-    index++;
-    if (index > fullText.length) {
-        isDeleting = true;
-        setTimeout(typeEffect, 1000); // pause avant suppression
-        return;
+  function typeEffect() {
+    if (index <= fullText.length) {
+      span.textContent = fullText.slice(0, index);
+      index++;
+      setTimeout(typeEffect, 150); // vitesse d’écriture
     }
-    } else {
-    span.textContent = fullText.slice(0, index);
-    index--;
-    if (index < 0) {
-        isDeleting = false;
-        setTimeout(typeEffect, 500); // pause avant réécriture
-        return;
-    }
-    }
-    setTimeout(typeEffect, 200); // vitesse d’écriture
-}
+  }
 
-typeEffect();
+  typeEffect();
 });
-
