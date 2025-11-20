3542ba9cb64f76e83a34b1f9fc8925169a75a9af
Amélie Bonhomme <ameliebonhomme7@gmail.com>
2025-11-20 19:14:43 +0100

    page veille ok

--- src/Controller/VeilleController.php
+++ src/Controller/VeilleController.php
@@ -1,8 +1,10 @@
 <?php
 
+// src/Controller/VeilleController.php
 namespace App\Controller;
-use App\Service\YoutubeService;
 
+use App\Service\YoutubeService;
+use App\Service\HackerNewsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,14 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class VeilleController extends AbstractController
 {
     #[Route('/veille', name: 'veille')]
-    public function index(YoutubeService $youtube): Response
+    public function index(YoutubeService $youtube, HackerNewsService $hackerNews): Response
     {
         $channelId = 'UCWedHS9qKebauVIK2J7383g';
         $videos = $youtube->getLatestVideos($channelId, 4);
+        $articles = $hackerNews->getLatestArticles(4);
 
         return $this->render('home/veille.html.twig', [
             'videos' => $videos,
+            'articles' => $articles,
         ]);
     }
 }
+
+
 
